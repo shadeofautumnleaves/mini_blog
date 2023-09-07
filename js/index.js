b0cd8f54cca193705e69545e88a15b528e3d8563
shadeofautumnleaves <weak.designer@gmail.com>
2023-09-07 19:33:07 +0600

    added html/scss/js

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.querySelector(".burger-lines").addEventListener("click", () => {
+    document.querySelector(".burger-menu").classList.toggle("active");
+    document.querySelector("body").classList.toggle("active");
+});
